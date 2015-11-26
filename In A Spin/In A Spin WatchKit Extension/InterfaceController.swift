282d46a4aebf52524c83740cdfada774337e41ce
Mark Goody <mgoody@shopkeep.com>
2015-11-25 21:46:40 -0500

    Use the button to start and stop the animation

--- In A Spin/In A Spin WatchKit Extension/InterfaceController.swift
+++ In A Spin/In A Spin WatchKit Extension/InterfaceController.swift
@@ -7,8 +7,18 @@ class InterfaceController: WKInterfaceController {
     @IBOutlet var spinnerImage: WKInterfaceImage!
     @IBOutlet var button: WKInterfaceButton!
     
+    private var animating = false
+    
     @IBAction func buttonTapped() {
-        spinnerImage.startAnimating()
+        if animating {
+            spinnerImage.stopAnimating()
+            button.setTitle("Spin")
+            animating = false
+        } else {
+            spinnerImage.startAnimating()
+            button.setTitle("Freeze")
+            animating = true
+        }
     }
     
     override func awakeWithContext(context: AnyObject?) {
