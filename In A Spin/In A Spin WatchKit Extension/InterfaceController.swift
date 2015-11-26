93398112e41bbc90e5e005b5156aaaf2976db835
Mark Goody <mgoody@shopkeep.com>
2015-11-25 21:51:10 -0500

    ANimate the button states

--- In A Spin/In A Spin WatchKit Extension/InterfaceController.swift
+++ In A Spin/In A Spin WatchKit Extension/InterfaceController.swift
@@ -12,12 +12,12 @@ class InterfaceController: WKInterfaceController {
     @IBAction func buttonTapped() {
         if animating {
             spinnerImage.stopAnimating()
-            button.setTitle("Spin")
             animating = false
+            animateWithDuration(0.2, animations: updateButtonToStopped)
         } else {
             spinnerImage.startAnimating()
-            button.setTitle("Freeze")
             animating = true
+            animateWithDuration(0.2, animations: updateButtonToGoing)
         }
     }
     
@@ -26,6 +26,21 @@ class InterfaceController: WKInterfaceController {
         
         let spinnerAnimation = UIImage.animatedImageNamed("spinner", duration: 2)!
         spinnerImage.setImage(spinnerAnimation)
+        updateButtonToStopped()
+    }
+    
+    private func updateButtonToStopped() {
+        let goColor = UIColor.init(red: 4/255, green: 222/255, blue: 13/255, alpha: 0.28)
+        
+        button.setBackgroundColor(goColor)
+        button.setTitle("Spin")
+    }
+    
+    private func updateButtonToGoing() {
+        let stopColor = UIColor.init(red: 250/255, green: 17/255, blue: 79/255, alpha: 0.34)
+        
+        button.setBackgroundColor(stopColor)
+        button.setTitle("Freeze")
     }
 
 }
