55d9e74ab2ad200dd97d95dcf98c31ce74015f98
Mark Goody <mgoody@shopkeep.com>
2015-11-25 21:41:59 -0500

    Add the image and the button

--- In A Spin/In A Spin WatchKit Extension/InterfaceController.swift
+++ In A Spin/In A Spin WatchKit Extension/InterfaceController.swift
@@ -4,6 +4,11 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var spinnerImage: WKInterfaceImage!
+    @IBOutlet var button: WKInterfaceButton!
     
+    @IBAction func buttonTapped() {
+        
+    }
 
 }
